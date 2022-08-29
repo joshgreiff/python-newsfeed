2c69e3f53fe4a009eec5fad9400c705dff037da2
Josh Greiff <josh@greiff.org>
2022-08-29 13:32:16 -0400

    finished homepage views and filters

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -4,8 +4,12 @@ def format_date(date):
 def format_url(url):
     return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
 
+def format_plural(amount, word):
+    if amount != 1:
+        return word + 's'
+    
+    return word
+
 from datetime import datetime
 print(format_date(datetime.now()))
 
-print(format_url('http://google.com/test/'))
-print(format_url('https://www.google.com?q=test'))
